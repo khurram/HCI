4af5750d5030d5b8417a0fbede6132c204e41504
Khurram <khurram@aslam.me>
2011-10-15 23:43:19 +0100

    Revert "Revert 299cda7134516bfb293223be8301dc2c4a41f267^..HEAD"
    This reverts commit 3c70f396ca403b093530fa50b8949df456ffdfde.

--- src/LabelFrame.java
+++ src/LabelFrame.java
@@ -1,4 +1,3 @@
-
 import javax.swing.JButton;
 import javax.swing.JInternalFrame;
 import javax.swing.JPanel;
@@ -11,7 +10,6 @@ import java.awt.FlowLayout;
 import java.awt.event.*;
 import java.beans.PropertyVetoException;
 
-
 /**
  * HCI Project Phase 1
  * 
@@ -20,8 +18,10 @@ import java.beans.PropertyVetoException;
 
 public class LabelFrame extends JInternalFrame implements ActionListener, KeyListener, InternalFrameListener{
 	private static final long serialVersionUID = 1L;
+	
 	private JTextField text;
 	ImageDesktop parent;
+	
     public LabelFrame(ImageDesktop parent,int x, int y) {
         super("Choose a Label", false, true, false, false); 
         this.parent = parent;
@@ -124,9 +124,11 @@ public class LabelFrame extends JInternalFrame implements ActionListener, KeyLis
 		
 		
 	}
+	
 	private void addLabel(String label) {
 		parent.addLabel(label);
 	}
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		if("submit".equals(e.getActionCommand())) {
@@ -140,7 +142,6 @@ public class LabelFrame extends JInternalFrame implements ActionListener, KeyLis
 				System.out.println("Fatal error");
 			}
 			dispose();
-		}
-		
+		}	
 	}
 }
